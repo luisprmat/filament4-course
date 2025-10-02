af9796cd0ac440d449687a3443f12583f6b9d5cc
Luis Parrado <luisprmat@gmail.com>
2025-10-01 21:12:58 -0500

    First CRUD Menu: Product Resource

---
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('price'),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\Field;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TextColumn::configureUsing(function (TextColumn $column) {
+            $column->translateLabel();
+        });
+
+        Field::configureUsing(function (Field $field) {
+            $field->translateLabel();
+        });
     }
 }
